edd144d9b1392b3963d80501a7f5040f10973695
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-08-08 12:25:28 +0100

    feat: add mysql adapter

---
+++ src/adapters/database/mysql.js
@@ -0,0 +1,21 @@
+import mysql from 'mysql'
+import { promisify } from 'util'
+
+const createConnection = () => {
+  return mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    database: process.env.DB,
+    password: process.env.PASS,
+    multipleStatements: true
+  })
+}
+
+const conn = createConnection()
+
+export const connection = {
+  query: promisify(conn.query).bind(conn),
+  close () {
+    conn.destroy()
+  }
+}
