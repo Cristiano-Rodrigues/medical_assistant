0a3de6bbbfa033ff52907e8f990ba8d97dbc34d3
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-08-11 09:00:00 +0100

    test: add test for create patient repository

---
+++ src/repositories/patient.test.js
@@ -0,0 +1,18 @@
+import { patientRepository } from './patient'
+import { connection } from '../adapters/database/mysql'
+import { req } from '../test/patient'
+
+const patient = {
+  ...req.body,
+  gender: Math.random() > 0.5 ? 'm' : 'f'
+}
+const { create } = patientRepository(connection)
+
+describe('patientRepository', () => {
+  test('Should insert patient in the database', async () => {
+    const { insertId } = await create(patient)
+    const [ insertPatient ] = await connection.query(`SELECT * FROM patient WHERE id = ?`, [insertId])
+
+    expect(insertPatient).not.toBe(undefined)
+  })
+})
