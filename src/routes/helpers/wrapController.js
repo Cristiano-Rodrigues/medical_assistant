309ae1686fcb4764db10dcac07f9124991665ea3
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-08-11 14:55:31 +0100

    feat: log errors and don't send errors below 500 to client

--- src/routes/helpers/wrapController.js
+++ src/routes/helpers/wrapController.js
@@ -1,6 +1,14 @@
+import { ServerError } from '../../errors'
+
 export function wrapController (controller, params) {
   return async (req, res) => {
     const result = await controller(req, params)
-    res.status(result.code).send(result)
+    console.log(`Request "${controller.name}". Result: `, result)
+
+    res.status(result.code)
+    if (result.code >= 500) {
+      return res.send({ error: new ServerError() })
+    }
+    res.send(result)
   }
 }
