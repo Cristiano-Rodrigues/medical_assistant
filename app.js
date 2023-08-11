23d89f00c41e414ce63d07de3018b3a4515bb1e1
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-08-11 11:42:10 +0100

    feat: add more configurations for server

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import fs from 'fs'
+import path from 'path'
 
 dotenv.config()
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use((_, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  next()
+})
+
+function getRouteFiles (src) {
+  return fs.readdirSync(src).filter(file => (
+    fs.statSync(`${src}${file}`).isFile()
+  ))
+}
+
+async function load (filename) {
+  const src = './src/routes/' + filename
+  const exported = (await import(src)).default
+  const router = express.Router()
+  
+  return exported(router)
+}
+
+function registerRoute (route) {
+  app.use('/api/v1', route)
+}
+
+const src = path.join(process.cwd(), '/src/routes/')
+
+Promise.all(getRouteFiles(src).map(load))
+  .then(result => result.forEach(registerRoute))
 
 export default app
