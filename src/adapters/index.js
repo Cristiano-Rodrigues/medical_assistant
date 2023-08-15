bf2d90f560cf3b54879a0d1613e243acab25d5b8
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-08-15 14:03:08 +0100

    feat: add adapter for express-validator

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -1,3 +1,4 @@
 export * from './database/mysql'
 export * from './hasher/bcrypt'
 export * from './mailer/nodemailer'
+export * from './validator/express-validator'
---
+++ src/adapters/validator/express-validator.js
@@ -0,0 +1,23 @@
+import { validationResult } from 'express-validator'
+import { InvalidEntry } from '../../errors'
+
+export const validate = validations => {
+  return async (req, res, next) => {
+    for (let validation of validations) {
+      const result = await validation.run(req)
+      if (result.errors.length) break
+    }
+
+    const errors = validationResult(req)
+    if (errors.isEmpty()) {
+      return next()
+    }
+    
+    const field = errors.array()[0].path
+
+    res.status(400).send({
+      code: 400,
+      error: new InvalidEntry(field)
+    })
+  }
+}
