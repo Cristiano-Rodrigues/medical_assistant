791de555a742cd356f78af79cc6177807ed07388
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-08-15 14:25:53 +0100

    feat: add validators for signup entries

---
+++ src/routes/validators/signup/signup.js
@@ -0,0 +1,12 @@
+import { body } from 'express-validator'
+import { validate } from '../../../adapters'
+
+export const validateSignUp = validate([
+  body('name').isString().notEmpty(),
+  body('gender').isIn(['m', 'f']),
+  body('born').isISO8601(),
+  body('address').optional().isString(),
+  body('phone').optional().isString(),
+  body('email').isEmail(),
+  body('password').isLength({ min: 8 })
+])
