32a6d869afcf40fb43905a10c996c3dfdb5b8eae
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-08-18 17:22:16 +0100

    test: add new http  response functions

--- src/controllers/helpers/http.test.js
+++ src/controllers/helpers/http.test.js
@@ -1,24 +1,38 @@
-import { InvalidEntry, ServerError } from '../../errors'
-import { invalidEntry, serverError, success } from './http'
+import { DuplicatedEntry, InvalidEntry, ServerError, Unauthorized } from '../../errors'
+import { HttpStatusCodes, duplicatedEntry, invalidEntry, serverError, success, unauthorized } from './http'
 
 describe('http.js', () => {
   test('invalidEntry should return correct http response', () => {
     expect(invalidEntry('any_field')).toEqual({
-      code: 400,
+      code: HttpStatusCodes.BadRequest,
       error: new InvalidEntry('any_field')
     })
   })
 
   test('serverError should return correct http response', () => {
-    expect(serverError()).toEqual({
-      code: 500,
+    expect(serverError(new ServerError())).toEqual({
+      code: HttpStatusCodes.ServerError,
       error: new ServerError()
+    })
+  })
+
+  test('duplicatedEntry should return correct http response', () => {
+    expect(duplicatedEntry('field')).toEqual({
+      code: HttpStatusCodes.BadRequest,
+      error: new DuplicatedEntry('field')
+    })
+  })
+
+  test('unauthorized should return correct http response', () => {
+    expect(unauthorized()).toEqual({
+      code: HttpStatusCodes.Unauthorized,
+      error: new Unauthorized()
     })
   })
 
   test('success should return correct http response', () => {
     expect(success()).toEqual({
-      code: 200,
+      code: HttpStatusCodes.Ok,
       success: true
     })
   })
