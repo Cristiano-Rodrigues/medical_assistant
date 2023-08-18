dcbf12dd70df84227faa1d9f70096d67da80a921
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-08-18 18:03:20 +0100

    feat: add validator for login

---
+++ src/routes/validators/login.js
@@ -0,0 +1,7 @@
+import { body } from 'express-validator'
+import { validate } from '../../adapters'
+
+export const validateLogin = validate([
+  body('email').isEmail(),
+  body('password').isLength({ min: 8 })
+])
