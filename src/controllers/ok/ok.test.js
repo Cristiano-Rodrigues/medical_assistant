cb2c042f87165cc8bc69f026d9c3b26dded7a0a6
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-08-28 08:02:00 +0100

    test: add test for route ok

---
+++ src/controllers/ok/ok.test.js
@@ -0,0 +1,9 @@
+import { success } from '../helpers'
+import { ok } from './ok'
+
+describe('ok', () => {
+  test('Should return correct answer', async () => {
+    const result = await ok()
+    expect(result).toEqual(success())
+  })
+})
