b48db828c16ceb0e658451a0ca74cea3884352f3
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-09-19 14:10:15 +0100

    refactor: refactor filterUser function

--- src/controllers/helpers/filterUser.js
+++ src/controllers/helpers/filterUser.js
@@ -1,13 +1,4 @@
-export function filterUser (user) {
-  let output = {}
-  const filterFields = [
-    'password',
-    'created_at',
-    'updated_at'
-  ]
-  Object.keys(user).filter(field => (
-    !filterFields.includes(field)
-  )).forEach(field => output[field] = user[field])
-
-  return output
+export const filterUser  = (
+  { password, created_at, updated_at, ...user }) => {
+    return user
 }
