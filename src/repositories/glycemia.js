a41453e7756fafacd9a98c974e3429deb22bff7a
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-09-19 15:08:34 +0100

    feat: add new repository

---
+++ src/repositories/glycemia.js
@@ -0,0 +1,26 @@
+export function patientRepository (connection) {
+  async function register ({
+    level,
+    patientId,
+    date,
+    observation
+  }) {
+    const sql = {
+      query: 'INSERT INTO glycemia VALUES (default, ?, ?, ?, ?, default, default)',
+      values: [level, date, observation, patientId]
+    }
+    return await connection.query(sql.query, sql.values)
+  }
+
+  async function register (email) {
+    const sql = {
+      query: 'SELECT * FROM patient WHERE email = ? LIMIT 1',
+      values: [email]
+    }
+    return (await connection.query(sql.query, sql.values))[0]
+  }
+
+  return {
+    register
+  }
+}
