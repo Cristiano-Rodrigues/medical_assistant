a8a368eec9098364a7ea538c59316bef50ea618d
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-09-19 17:41:36 +0100

    fix: remove unnecessary tests

--- src/middlewares/helpers/http.test.js
+++ src/middlewares/helpers/http.test.js
@@ -1,30 +1,7 @@
-import { DuplicatedEntry, InvalidEntry, ServerError, Unauthorized } from '../../errors'
-import { HttpStatusCodes, duplicatedEntry, invalidEntry, serverError, success, unauthorized } from './http'
+import { Unauthorized } from '../../errors'
+import { HttpStatusCodes, success, unauthorized } from './http'
 
 describe('http.js', () => {
-  test('invalidEntry should return correct http response', () => {
-    expect(invalidEntry('any_field')).toEqual({
-      code: HttpStatusCodes.BadRequest,
-      error: new InvalidEntry('any_field'),
-      location: 'any_field'
-    })
-  })
-
-  test('serverError should return correct http response', () => {
-    expect(serverError(new ServerError())).toEqual({
-      code: HttpStatusCodes.ServerError,
-      error: new ServerError()
-    })
-  })
-
-  test('duplicatedEntry should return correct http response', () => {
-    expect(duplicatedEntry('field')).toEqual({
-      code: HttpStatusCodes.BadRequest,
-      error: new DuplicatedEntry('field'),
-      location: 'field'
-    })
-  })
-
   test('unauthorized should return correct http response', () => {
     expect(unauthorized()).toEqual({
       code: HttpStatusCodes.Unauthorized,
