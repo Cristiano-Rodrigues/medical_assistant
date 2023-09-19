7e4c92a026859fad54c8613e5f671858524aedb6
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-09-19 17:27:07 +0100

    fix: use helper functions

--- src/middlewares/authentication/jwtAuth.js
+++ src/middlewares/authentication/jwtAuth.js
@@ -1,35 +1,21 @@
-import { Unauthorized } from '../../errors'
-
-const unauthorized = {
-  code: 401,
-  error: new Unauthorized()
-}
+import { getToken, unauthorized, success } from '../helpers'
 
 export function auth (req, { jwt }) {
-  const token = getToken(req.headers.authorization)
-
-  if (!token) {
-    return unauthorized
-  }
-
   try {
+    const token = getToken(req.headers.authorization)
+  
+    if (!token) {
+      return unauthorized()
+    }
+
     const payload = jwt.verify(token)
 
     if (!payload) {
-      return unauthorized
+      return unauthorized()
     }
   } catch (error) {
-    return unauthorized
+    return unauthorized()
   }
 
-  return {
-    success: true
-  }
-}
-
-function getToken (authHeader) {
-  const regexp = /Bearer ([\w\.]+)/
-  const [, token] = authHeader.match(regexp)
-
-  return token
+  return success()
 }
