1b0d626a783a9279e207a16c3ff1a2acd8232b26
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-09-19 15:15:17 +0100

    feat: add validator for registerGlycemia endpoint

---
+++ src/routes/validators/registerGlycemia.js
@@ -0,0 +1,8 @@
+import { body } from 'express-validator'
+import { validate } from '../../adapters'
+
+export const validateRegisterGlycemia = validate([
+  body('level').isNumeric().notEmpty(),
+  body('patientId').isNumeric().notEmpty(),
+  body('observation').optional().isString()
+])
