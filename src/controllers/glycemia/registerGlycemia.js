842a94e6da55aa8c46900f05faf532b3eff74795
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-09-19 14:50:07 +0100

    feat: add controller to register level of glycemia

---
+++ src/controllers/glycemia/registerGlycemia.js
@@ -0,0 +1,41 @@
+import { invalidEntry, serverError, success } from '../helpers'
+
+export async function registerGlycemia (req, {
+  Connection,
+  patientRepository,
+  glycemiaRepository
+}) {
+  const {
+    level,
+    patientId,
+    observation
+  } = req.body
+
+  let connection
+
+  try {
+    connection = new Connection()
+    const { getById } = patientRepository(connection)
+
+    const patient = await getById(patientId)
+    if (!patient) {
+      return invalidEntry('patientId')
+    }
+
+    const { register } = glycemiaRepository(connection)
+    const date = new Date()
+
+    await register({
+      level,
+      patientId,
+      date,
+      observation
+    })
+
+    return success()
+  } catch (error) {
+    return serverError(error)
+  } finally {
+    connection.close()
+  }
+}
