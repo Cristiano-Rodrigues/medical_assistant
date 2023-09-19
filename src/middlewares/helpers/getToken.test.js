ed70a9fa4ace6f9bf886e02c1847942cda1999a7
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-09-19 17:26:38 +0100

    test: add helper function to extract token from req.header

---
+++ src/middlewares/helpers/getToken.test.js
@@ -0,0 +1,18 @@
+import { getToken } from './getToken'
+
+
+describe('getToken', () => {
+  test('Should return null if an invalid argument is given', () => {
+    expect(getToken()).toBe(null)
+    expect(getToken({})).toBe(null)
+  })
+
+  test('Should return null if passed string has wrong format', () => {
+    expect(getToken('an_invalid_string_format')).toBe(null)
+  })
+
+  test('Should return token if a valid argument is given', () => {
+    const token = getToken('Bearer example_token')
+    expect(token).toBe('example_token')
+  })
+})
