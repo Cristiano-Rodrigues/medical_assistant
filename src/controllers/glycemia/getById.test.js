a119fe933bf05624d05f5e324975573e57e40746
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-09-25 16:52:04 +0100

    test: add controller to get glycemia data by id

---
+++ src/controllers/glycemia/getById.test.js
@@ -0,0 +1,40 @@
+import { ServerError } from '../../errors'
+import { serverError, success } from '../helpers'
+import { getGlycemiaById } from './getById'
+
+class Connection {
+  close () {}
+}
+const glycemiaRepository = (conn) => ({
+  getById: async () => ({})
+})
+const req = {
+  body: {
+    id: 1
+  }
+}
+
+describe('getGlycemiaById', () => {
+  test('Should return server error if something goes wrong', async () => {
+    const glycemiaRepository = (conn) => ({
+      getById: async () => {
+        throw new ServerError()
+      }
+    })
+    const result = await getGlycemiaById(req, {
+      Connection,
+      glycemiaRepository
+    })
+    expect(result).toEqual(serverError(new ServerError()))
+  })
+
+  test('Should return result if no errors', async () => {
+    const result = await getGlycemiaById(req, {
+      Connection,
+      glycemiaRepository
+    })
+    expect(result).toEqual(success({
+      result: {}
+    }))
+  })
+})
