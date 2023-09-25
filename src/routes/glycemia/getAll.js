4c841b504dd888ac8776cb6e95a557b822f5c5b1
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-09-25 16:35:32 +0100

    feat: add route to get data about glycemia

---
+++ src/routes/glycemia/getAll.js
@@ -0,0 +1,20 @@
+import { getAllGlycemia } from '../../controllers/glycemia/getAll'
+import { auth } from '../../middlewares'
+import { glycemiaRepository } from '../../repositories'
+import { wrapController, wrapMiddleware, Connection, jwt } from '../../adapters'
+
+export default router => {
+  const params = {
+    Connection,
+    glycemiaRepository
+  }
+  const authMiddleware = wrapMiddleware(auth, { jwt })
+
+  router.get(
+    '/glycemia',
+    authMiddleware,
+    wrapController(getAllGlycemia, params)
+  )
+
+  return router
+}
