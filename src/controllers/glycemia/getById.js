46c734d8692f58e3869d88ab61e46b72f1dbfaec
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-09-25 16:51:32 +0100

    feat: add controller to get glycemia data by id

---
+++ src/controllers/glycemia/getById.js
@@ -0,0 +1,22 @@
+import { serverError, success } from '../helpers'
+
+export async function getGlycemiaById (req, {
+  Connection,
+  glycemiaRepository
+}) {
+  const id = req.body.id
+  let connection
+
+  try {
+    connection = new Connection()
+    const { getById } = await glycemiaRepository(connection)
+
+    const result = await getById(id)
+
+    return success({ result })
+  } catch (error) {
+    return serverError(error)
+  } finally {
+    connection.close()
+  }
+}
