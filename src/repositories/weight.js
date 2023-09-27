5c1889fedc900a145f23394f9b72fecac0d37059
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-09-27 12:39:17 +0100

    feat: add repository to get all registered weight data

--- src/repositories/weight.js
+++ src/repositories/weight.js
@@ -13,7 +13,17 @@ export function weightRepository (connection) {
     return await connection.query(sql.query, sql.values)
   }
 
+  async function getAll(patientId) {
+    const sql = {
+      query: 'SELECT w.* FROM weight AS w JOIN patient AS p ON ' +
+        'w.patient_id = p.id WHERE p.id = ? LIMIT 1000',
+      values: [patientId]
+    }
+    return await connection.query(sql.query, sql.values)
+  }
+
   return {
-    register
+    register,
+    getAll
   }
 }
